24f36255c08ea217bad06ea3809c5a82ff404929
Kumudu Gunasekara <kumudu.gunasekara@ICAREHEALTH.COM>
2016-07-04 10:29:12 +1000

    implementation

---
+++ app/main.ctrl.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('mainController', mainController);
+
+    mainController.$inject = ['userSvc'];
+
+    function mainController(user) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'mainController';
+
+        vm.user = new user('fname', 'lname', new Date());
+
+        
+    }
+})();
---
+++ app/user.svc.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('userSvc', userSvc);
+
+    userSvc.$inject = [];
+
+    function userSvc() {
+        function user(fname, lname, dob) {
+            this.fullName = fname + ' ' + lname;
+            this.dob = dob;
+            this.age = 25;
+        }
+
+        return user;
+    }
+})();
